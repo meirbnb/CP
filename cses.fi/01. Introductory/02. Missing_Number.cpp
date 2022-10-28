0ca0a0926a1895c8ddb4fbf2635b5470264d1985
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-10-28 23:27:38 +0300

    Update 02. Missing_Number.cpp

--- cses.fi/01. Introductory/02. Missing_Number.cpp
+++ cses.fi/01. Introductory/02. Missing_Number.cpp
@@ -2,16 +2,16 @@
  
 using namespace std;
  
-int n, ans;
- 
 int main() {
+	int n;
 	cin >> n;
+	int ans = n;
 	for (int x, i=1; i<n; i++){
 		cin >> x;
 		ans += i - x;
 	}
-	cout << ans + n << '\n';
+	cout << ans << '\n';
 	return 0;
 }
-
+ 
 // https://cses.fi/problemset/task/1083
