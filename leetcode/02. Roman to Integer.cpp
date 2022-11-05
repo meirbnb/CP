d1c79f80c0f1c84da94a48d3c9b4fe43862ada97
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-11-05 16:42:17 +0300

    Update 02. Roman to Integer.cpp

--- leetcode/02. Roman to Integer.cpp
+++ leetcode/02. Roman to Integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
+        int ans = 0, nxt = 0;
         map<char, int>m;
         m['I'] = 1;
         m['V'] = 5;
@@ -10,7 +10,6 @@ public:
         m['C'] = 100;
         m['D'] = 500;
         m['M'] = 1000;
-        int nxt = 0;
         for (int i=0; i<s.length(); i++){
             int cur  = m[s[i]];
             int nxt  = m[s[i+1]];
