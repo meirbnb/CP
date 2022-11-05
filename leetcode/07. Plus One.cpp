9502837b6774e05faecfa230168faf775eaf15a9
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-11-05 16:43:54 +0300

    Update 07. Plus One.cpp

--- leetcode/07. Plus One.cpp
+++ leetcode/07. Plus One.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int c = 1;
+        int one = 1;
         for (int i=digits.size()-1; i>=0; i--)
-            if (digits[i] + c > 9)
+            if (digits[i] + one > 9)
                 digits[i] = 0;
             else
-                digits[i] += c, c = 0;
-        if (c)
-            digits.insert(digits.begin(), c);
+                digits[i] += one, one = 0;
+        if (one)
+            digits.insert(digits.begin(), one);
         return digits;
     }
 };
