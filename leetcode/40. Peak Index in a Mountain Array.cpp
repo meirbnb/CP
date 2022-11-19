635d5ebb5d6cd4b0fdc7f485076b05dd0190e73f
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-11-19 15:49:58 +0300

    Add files via upload

---
+++ leetcode/40. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+        int solve(vector<int>a, int l, int r){
+	        if (l <= r){
+	        
+	            if (l == r)
+	                return r;
+	            
+	            int m = l + (r - l) / 2;
+	
+	            if (a[m - 1] < a[m] && a[m] > a[m + 1])
+			        return m;
+	
+	            if (a[m - 1] < a[m] && a[m] < a[m + 1])
+			        return solve(a, m, r);
+	
+	            if (a[m - 1] > a[m] && a[m] > a[m + 1])
+			        return solve(a, l, m);
+	    
+	        }
+
+    	    return r;
+    }
+    int peakIndexInMountainArray(vector<int>& arr) {
+        return solve(arr, 0, arr.size() - 1);
+    }
+};
+
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
