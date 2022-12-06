d4870dead13c37b1ba0528240ff7964873e2fbc0
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-12-06 20:45:49 +0300

    Add files via upload

---
+++ AdventOfCode22/day6_2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define ll long long
+
+int ans;
+
+void solve(){
+	string s;
+	cin >> s;
+	int N = s.length();
+	int K = 14;
+	for (int i=0; i<N-K+1; i++){
+		string cur = s.substr(i, K);
+		set<char>st(cur.begin(), cur.end());
+		if (st.size() == K){
+			cout << i + K << '\n';
+			return;
+		}
+	}
+	cout << "nothing found!\n";
+}	
+
+int main(){
+	int tt = 1;
+	//cin >> tt;
+	while(tt--)
+		solve();
+	return 0;
+}
